8edc5bd9d88cd013a9e8b94762818a353748a3ae
Chu <tchu4@lion.lmu.edu>
2018-10-04 12:57:06 -0700

    Began controllerbehaviour script.

---
+++ NebulaVR/Assets/Scripts/NebulaVR/View/ControllerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerBehaviour : MonoBehaviour {
+
+    public Vector3 PositionOffset;
+    //public SteamVR_TrackedObject Controller;
+
+    void OnTriggerEnter (Collider other)
+    {
+        //var device = SteamVR_Controller.Input((int)Controller.index);
+        //device.TriggerHapticPulse(500, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+        other.attachedRigidbody.position = new Vector3(20, 20, 20);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
