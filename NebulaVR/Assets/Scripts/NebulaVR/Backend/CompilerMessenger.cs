221a27e0d82511b3d10dd233b1299b64079651eb
cazinge <eddie.azinge@gmail.com>
2018-10-08 14:30:53 -0700

    Creating test client in Unity

--- NebulaVR/Assets/Scripts/NebulaVR/Backend/CompilerMessenger.cs
+++ NebulaVR/Assets/Scripts/NebulaVR/Backend/CompilerMessenger.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WebSocketSharp;
 
-public class CompilerMessenger : MonoBehaviour {
+public class CompilerMessenger : MonoBehaviour
+{
+  // Use this for initialization
+  void Start()
+  {
+    SetupClient();
+  }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void SetupClient()
+  {
+    var ws = new WebSocket("ws://localhost:5050");
+    ws.OnMessage += (sender, e) =>
+      Debug.Log("Recieved from Server: " + e.Data);
+    ws.Connect();
+    ws.Send("Connected from Unity!");
+  }
 }
