e1ea6175dc699a4c0ab89557033556460e1439a1
Justin Torres <jtorre39@lion.lmu.edu>
2018-10-08 19:49:52 -0700

    Initial classes.

---
+++ NebulaVR/Assets/Scripts/NebulaVR/View/NebulaEnvironment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NebulaEnvironment
+{
+    private readonly LinkedList<Component> components = new LinkedList<Component>();
+
+    public Component CreateComponent()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DeleteComponent()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SerializeEnvironment()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void LoadEnvironment()
+    {
+        throw new System.NotImplementedException();
+    }
+}
