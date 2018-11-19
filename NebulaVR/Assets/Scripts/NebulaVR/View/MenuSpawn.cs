f75a6569b79df6f29d786be1cf0ddb68439e77a3
Smith <bsmith70@lion.lmu.edu>
2018-11-18 19:00:47 -0800

    Work in progress for menu w/ controllers.

---
+++ NebulaVR/Assets/Scripts/NebulaVR/View/MenuSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSpawn : MonoBehaviour {
+	public float spawnDistance = 3;
+	public GameObject player;
+	
+	void Start() {
+		Vector3 playerPosition = player.transform.position;
+		Vector3 playerDirection = player.transform.forward;
+		transform.position = playerPosition + playerDirection * spawnDistance;
+	}
+}
