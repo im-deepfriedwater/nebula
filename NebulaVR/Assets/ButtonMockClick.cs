52009680e1709c220da1ed581b39b45fa528b184
Smith <bsmith70@lion.lmu.edu>
2018-11-27 21:25:03 -0800

    Added mock functionality to all the buttons

---
+++ NebulaVR/Assets/ButtonMockClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMockClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onClick () {
+		Debug.Log("Button pressed");
+	}
+}
