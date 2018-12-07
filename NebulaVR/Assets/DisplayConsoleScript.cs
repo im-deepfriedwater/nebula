5f3344138f7a842c04630f867fc64cc87ab9c5e2
Edmiston <tedmist1@lion.lmu.edu>
2018-12-07 04:01:13 -0800

    Console prefab + fixes to the menu and keypad

---
+++ NebulaVR/Assets/DisplayConsoleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DisplayConsoleScript : MonoBehaviour {
+
+    string value;
+    public Text currentText;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentText.text = value;
+	}
+
+    void setValue(string val)
+    {
+        value = val;
+    }
+
+    string getValue()
+    {
+        return value;
+    }
+}
