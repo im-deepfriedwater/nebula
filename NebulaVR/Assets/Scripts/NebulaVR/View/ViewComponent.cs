f3a094d495a6b013a2d9fd480cb2c8af68d5af3a
cazinge <eddie.azinge@gmail.com>
2018-12-06 22:33:22 -0800

    Initial draft for initialize

--- NebulaVR/Assets/Scripts/NebulaVR/View/ViewComponent.cs
+++ NebulaVR/Assets/Scripts/NebulaVR/View/ViewComponent.cs
@@ -2,57 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ViewComponent: ViewConstruct
+public class ViewComponent : ViewConstruct
 {
-    // Should keep track of the game object it's attached to.
-    // Has to keep track of component links and relationships.
-    // Positions in space.
-    // Perhaps also the type of component it is.
-    // Inputs and outputs.
-    ViewBlock parent;
-    Vector3 internalPosition; // NOTE: This is relative to the parent.
-    ViewLink link;
-    public ComponentBinding binding;
-    [SerializeField]
-    public ComponentType componentType;
+  // Should keep track of the game object it's attached to.
+  // Has to keep track of component links and relationships.
+  // Positions in space.
+  // Perhaps also the type of component it is.
+  // Inputs and outputs.
+  ViewBlock parent;
+  Vector3 internalPosition; // NOTE: This is relative to the parent.
+  ViewLink link;
 
-    public void Initialize(ViewBlock parent, ComponentBinding binding)
-    {
-        this.parent = parent;
-        this.binding = binding;
-    }
+  // Internal Value that is used to determine the starting value 
+  // for Parameters without an entering Link. Default value is null.
+  string initializeValue;
+  public string InitializeValue
+  {
+    get { return initializeValue; }
+  }
 
-    // Should be called by the controller when a component is moved.
-    override public void SignifyChange()
-    {
-        UpdateInternals();
-        binding.PropagateChange();
-        parent.SignifyChange();
-    }
+  public ComponentBinding binding;
+  [SerializeField]
+  public ComponentType componentType;
 
-    void LinkComponent(ViewLink link)
-    {
-        this.link = link;
-        SignifyChange();
-    }
+  public void Initialize(ViewBlock parent, ComponentBinding binding)
+  {
+    this.parent = parent;
+    this.binding = binding;
+    this.initializeValue = null;
+  }
 
-    private void UpdateInternals()
-    {
-        this.internalPosition = gameObject.transform.localPosition;
-    }
+  // Should be called by the controller when a component is moved.
+  override public void SignifyChange()
+  {
+    UpdateInternals();
+    binding.PropagateChange();
+    parent.SignifyChange();
+  }
 
-    public override void Delete()
-    {
-        Destroy(gameObject);
-    }
+  void LinkComponent(ViewLink link)
+  {
+    this.link = link;
+    SignifyChange();
+  }
+
+  private void UpdateInternals()
+  {
+    this.internalPosition = gameObject.transform.localPosition;
+  }
+
+  public override void Delete()
+  {
+    Destroy(gameObject);
+  }
 }
 
 public enum ComponentType
 {
-    Origin,
-    Function,
-    Accessor,
-    Return,
-    Parameter,
-    Input
+  Origin,
+  Function,
+  Accessor,
+  Return,
+  Parameter,
+  Input
 }
